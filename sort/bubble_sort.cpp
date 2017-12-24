fdb611ec975312c7a00a63fcafb79b3ca2942b87
Kose-i <tamura.kosei905@mail.kyutech.jp>
2017-12-24 17:56:36 +0900

    first commit

---
+++ sort/bubble_sort.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+void sort(std::vector<int>& a)
+{
+  for(int check = 1;;check = 1){
+    for(int i = 0;i < a.size() - 1;++i)
+    {
+      if(a[i + 1] < a[i])
+      {
+        int b = a[i + 1];
+        a[i + 1] = a[i];
+        a[i] = b;
+        check = 0;
+      }
+    }
+    if(check)break;
+  }
+}
+int main()
+{
+  std::vector<int> target{3,2,4,5,3,7,1};
+  sort(target);
+  for(int& e : target)
+  {
+    std::cout << e << ' ';
+  }
+  std::cout << '\n';
+  return 0;
+}
