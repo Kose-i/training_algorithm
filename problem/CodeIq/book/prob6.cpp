c64bb9299e3f32262fb8f9ec8c40bd3733490259
Kose-i <tamura.kosei905@mail.kyutech.jp>
2019-02-17 16:27:04 +0900

    Add prob6

--- problem/CodeIq/book/prob6.cpp
+++ problem/CodeIq/book/prob6.cpp
@@ -1,19 +1,26 @@
 #include <iostream>
 
 namespace prob6{
-  class Box{
-    private:
-      unsigned long_line, short_line;
-    public:
-      Box(unsigned x, unsigned y) {
-        long_line = std::max(x,y);
-        short_line = std::min(x,y);
-      }
-      unsigned get_long_line() {return long_line;}
-      unsigned get_short_line() {return short_line;}
-  };
+  bool is_make_box(const unsigned& long_line, const unsigned& short_line, const unsigned& target_num) {
+    if (target_num == 1 && long_line == short_line) {
+      return true;
+    } else if (target_num == 1) {
+      return false;
+    } else {
+      unsigned surplus_long_line = std::max(long_line - short_line, short_line);
+      unsigned surplus_short_line = std::min(long_line - short_line, short_line);
+      return is_make_box(surplus_long_line, surplus_short_line, target_num - 1);
+    }
+  }
   unsigned answer() {
     unsigned ans_tmp {};
+    for (auto long_line = 1000;0 < long_line;--long_line) {
+      for (auto short_line = long_line - 1;0 < short_line;--short_line) {
+        if ( is_make_box(long_line, short_line, 20) ) {
+          ++ans_tmp;
+        }
+      }
+    }
     return ans_tmp;
   }
 }
