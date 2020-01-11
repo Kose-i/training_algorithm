6767845d70970a6493077505e6875d2b1ccdd4f8
Kose-i <tamura.kosei905@mail.kyutech.jp>
2020-01-11 14:23:10 +0900

    Add AtCoderLiveAnswer

--- problem/AtCoder/AtCoderBeginnerContest150/D.cpp
+++ problem/AtCoder/AtCoderBeginnerContest150/D.cpp
@@ -1,37 +1,7 @@
-#include <algorithm>
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
+
+using namespace std;
 
 int main(int argc, char** argv) {
-  unsigned long N;
-  unsigned long long M;
-  std::cin >> N >> M;
-  std::vector<unsigned long long> A(N);
-
-  for (auto& e : A) std::cin >> e;
-  std::sort(A.begin(), A.end());
-
-  std::vector<unsigned long> M_candidate;
-  for (unsigned long p=0;;++p) {
-    unsigned long num = A[N-1]*(p+0.5);
-    if (num > M) break;
-    M_candidate.push_back(num);
-  }
-  std::cout << "check\n";
-  unsigned long long cnt {};
-  for (const auto& num : M_candidate) {
-    bool is_check = true;
-    for (const auto& e : A) {
-      if (num%(e/2) != 0 || num/(e/2)%2==0) {
-        is_check = false;
-        break;
-      }
-    }
-    if (is_check == false) {
-      is_check = true;
-    } else {
-      ++cnt;
-    }
-  }
-  std::cout << cnt << '\n';
+  vector<int> a(2);
 }
