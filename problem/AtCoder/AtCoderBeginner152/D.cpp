44a63ac95cef72854f5bed897d3d4e3703ca2458
Kose-i <tamura.kosei905@mail.kyutech.jp>
2020-01-20 22:10:36 +0900

    Add D-E

--- problem/AtCoder/AtCoderBeginner152/D.cpp
+++ problem/AtCoder/AtCoderBeginner152/D.cpp
@@ -1,25 +1,50 @@
 #include <iostream>
 #include <vector>
 
-unsigned long add_path(const int& start, const int& end, const int& N) {
-  int tmp_N = N;
-  int cnt {1};
-  unsigned long sum {};
-
-  return sum;
+int get_keta(unsigned t) {
+  int ans {};
+  for (ans = 0;t > 9;t/=10, ++ans);
+  return (ans+1);
+}
+unsigned get_permu(const int& start, const int& end, const int& ketasuu, const int& N) {
+  if (ketasuu == 1) {
+    if (start != end) return 0;
+    return (N<start)?0:1;
+  }
+  unsigned tmp_num {1};
+  for (auto i = 1;i < ketasuu;++i) {
+    tmp_num *= 10;
+  }
+  int N_cp = N - start*tmp_num - end;
+  if (N_cp < 0) {
+    return 0;
+  } else if (N_cp > tmp_num) {
+    tmp_num = 1;
+    for (auto i = 0;i < ketasuu - 2;++i) {
+      tmp_num *= 10;
+    }
+    return tmp_num;
+  } else {
+    return 1+(N_cp/10);
+  }
 }
 int main(int argc, char** argv) {
-  int N;
+  unsigned N;
   std::cin >> N;
-  std::vector<std::vector<unsigned long>> dp(10, std::vector<unsigned long>(10, 0));
-  //dp[i][j] : 先頭i最後j
-  for (auto i = 1;i <= 9;++i) {
+  std::vector<std::vector<unsigned>> dp(10, std::vector<unsigned>(10, 0));
+  int ketasuu = get_keta(N);
+  for (auto i = 1;i <= ketasuu;++i) {
     for (auto j = 1;j <= 9;++j) {
-      if (i==j) {//1桁
-        if (N<i) break;
-        dp[i][j] = 1;
+      for (auto k = 1;k <= 9;++k) {
+        dp[j][k] += get_permu(j, k, i, N);
       }
-      dp[i][j] += add_path(i, j, N);
     }
   }
+  unsigned long sum {};
+  for (auto i = 1;i <= 9;++i) {
+    for (auto j = 1;j <= 9;++j) {
+      sum += dp[i][j]*dp[j][i];
+    }
+  }
+  std::cout << sum << '\n';
 }
--- problem/AtCoder/AtCoderBeginner152/E.cpp
+++ problem/AtCoder/AtCoderBeginner152/E.cpp
@@ -1,37 +1,67 @@
 #include <iostream>
 #include <vector>
+#include <bitset>
 
-constexpr unsigned mod_target {1000000007};
-unsigned gcd(const unsigned& a, const unsigned& b) {
-  if (b==0) return a;
-  return gcd(b, a%b);
+//constexpr int Max_num {1000000};
+constexpr int Max_num {10};
+constexpr int mod_target {1000000007};
+std::vector<int> prime_num() {
+  std::bitset<Max_num+1> prime;
+  for (auto i = 2;i <= Max_num;++i) {
+    prime[i] = 1;
+  }
+  for (auto i = 2;i < Max_num/2;++i) {
+    if (prime[i] == 0) continue;
+    for (auto j = 2;i*j <= Max_num;++j) {
+      prime[i*j] = 0;
+    }
+  }
+  std::vector<int> prime_box;
+  for (auto i = 2;i <= Max_num;++i) {
+    if (prime[i] == 1) prime_box.push_back(i);
+  }
+  return prime_box;
+}
+constexpr int max(int a, int b) {
+  return (a>b)?a:b;
 }
 
 int main(int argc, char** argv) {
   int N;
   std::cin >> N;
-  std::vector<unsigned> A(N);
+  std::vector<int> A(N);
   for (auto& e : A) std::cin >> e;
-  std::vector<unsigned long> B(N, 1);
+  std::vector<int> prime = prime_num();
+  int prime_box_size = prime.size();
+  std::vector<std::vector<int>> A_prime(N, std::vector<int>(prime_box_size, 0));
   for (auto i = 0;i < N;++i) {
-    unsigned tmp_cp = A[i];
-    for (auto j = 0;j < i;++j) {
-      B[j] *= tmp_cp;
-    }
-    for (auto j = i;j < N;++j) {
-      if (A[j] % tmp_cp == 0) {
-        A[j] /= tmp_cp;
-      } else {
-        unsigned gcd_kai = gcd(tmp_cp, A[j]);
-        B[j] *= (tmp_cp/gcd_kai)%mod_target;
-        B[j] %= mod_target;
-        A[j] /= gcd_kai;
+    for (auto j = 0;j < prime_box_size;++j) {
+      while (A[i]%prime[j] == 0) {
+        ++A_prime[i][j];
+        A[i] /= prime[j];
       }
+      if (A[i] == 1) break;
     }
   }
-  unsigned sum {};
-  for (auto& e : B) {
-    sum += (e%mod_target);
+  for (auto& e : prime) {
+    e %= mod_target;
+  }
+  std::vector<int> max_A_prime(prime_box_size, 0);
+  for (auto i = 0;i < prime_box_size;++i) {
+    for (auto j = 0;j < N;++j) {
+      max_A_prime[i] = max(max_A_prime[i], A_prime[j][i]);
+    }
+  }
+  unsigned long sum {};
+  for (auto i = 0;i < N;++i) {
+    unsigned long tmp_sum {1};
+    for (auto j = 0;j < prime_box_size;++j) {
+      for (int k = max_A_prime[j] - A_prime[i][j];k > 0;--k) {
+        tmp_sum *= prime[j];
+        tmp_sum %= mod_target;
+      }
+    }
+    sum += tmp_sum;
     sum %= mod_target;
   }
   std::cout << sum << '\n';
