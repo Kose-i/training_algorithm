db3b8fcfdb6d49b569edb6059f91e13181db5197
Kose-i <tamura.kosei905@mail.kyutech.jp>
2020-01-27 17:45:54 +0900

    Add ABC153-F

--- problem/AtCoder/AtCoderBeginnerContest153/F.cpp
+++ problem/AtCoder/AtCoderBeginnerContest153/F.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+/*
+提出 #9750607を参考にしました
+*/
 
 struct P {
   long long X;
   long long H;
 };
+
+constexpr unsigned long max_monsters_hp{1000000000};
 
 int main(int argc, char** argv) {
   long long N, D, A;
@@ -13,35 +18,24 @@ int main(int argc, char** argv) {
   std::vector<P> Monsters(N);
   for (auto& e : Monsters) std::cin >> e.X >> e.H;
   std::sort(Monsters.begin(), Monsters.end(), [](const auto& l, const auto& r){return (l.X<r.X);});
-  unsigned long cnt {};
-  int min_index {};
-  for (;;) {
-    int start_pos {};
-    for (start_pos = min_index;start_pos < N;++start_pos) {
-      if (Monsters[start_pos].H > 0) {
-        min_index = start_pos + 1;
-        break;
-      }
-    }
-    if (start_pos == N) break;
-    unsigned long long atack_power = A * (Monsters[start_pos].H/A);
-    cnt += Monsters[start_pos].H/A;
-    if (Monsters[start_pos].H % A != 0) {
-      atack_power += A;
-      ++cnt;
-    }
-    Monsters[start_pos].H = 0;
-    for (auto j = start_pos+1;j < N;++j) {
-      if (Monsters[j].X <= Monsters[start_pos].X + 2*D) {
-        if (Monsters[j].H < atack_power) {
-          Monsters[j].H = 0;
-        } else {
-          Monsters[j].H -= atack_power;
-        }
-      } else {
-        break;
-      }
+
+  long long ans {};
+  std::vector<long long> c(N+1);
+  for (auto i = 0;i < N;++i) {
+    c[i]   += Monsters[i].H;
+    c[i+1] -= Monsters[i].H;
+  }
+
+  for (auto i = 0;i < N;++i) {
+    if (i) c[i] += c[i-1];
+    if (c[i] > 0) {
+      long long cnt = (c[i] + A - 1) / A;
+      ans += cnt;
+      P tmp {Monsters[i].X + 2*D + 1, 0};
+      int index = std::lower_bound(Monsters.begin(), Monsters.end(), tmp, [](auto& l, auto& r){return l.X<r.X;}) - Monsters.begin();
+      c[i] -= cnt*A;
+      c[index] += cnt*A;
     }
   }
-  std::cout << cnt << '\n';
+  std::cout << ans << '\n';
 }
