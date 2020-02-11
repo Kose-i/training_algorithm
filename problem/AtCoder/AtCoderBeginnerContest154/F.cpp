49c2bcec9e78e469c47024eca26cf3f94379c413
Kose-i <tamura.kosei905@mail.kyutech.jp>
2020-02-11 18:14:59 +0900

    Add ABC154-F

--- problem/AtCoder/AtCoderBeginnerContest154/F.cpp
+++ problem/AtCoder/AtCoderBeginnerContest154/F.cpp
@@ -1,6 +1,61 @@
 #include <iostream>
 
+const int mod = 1000000007;
+struct mint {
+  ll x; // typedef long long ll;
+  mint(ll x=0):x((x%mod+mod)%mod){}
+  mint operator-() const { return mint(-x);}
+  mint& operator+=(const mint a) {
+    if ((x += a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator-=(const mint a) {
+    if ((x += mod-a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator*=(const mint a) {
+    (x *= a.x) %= mod;
+    return *this;
+  }
+  mint operator+(const mint a) const {
+    mint res(*this);
+    return res+=a;
+  }
+  mint operator-(const mint a) const {
+    mint res(*this);
+    return res-=a;
+  }
+  mint operator*(const mint a) const {
+    mint res(*this);
+    return res*=a;
+  }
+  mint pow(ll t) const {
+    if (!t) return 1;
+    mint a = pow(t>>1);
+    a *= a;
+    if (t&1) a *= *this;
+    return a;
+  }
+
+  // for prime mod
+  mint inv() const {
+    return pow(mod-2);
+  }
+  mint& operator/=(const mint a) {
+    return (*this) *= a.inv();
+  }
+  mint operator/(const mint a) const {
+    mint res(*this);
+    return res/=a;
+  }
+};
 int main(int argc, char** argv) {
   unsigned x_left, y_left, x_right, y_right;
   std::cin >> x_left >> y_left >> x_right >> y_right;
+  mint Ans {};
+  for (auto i = x_left;i <= x_right;++i) {
+    for (auto j = y_left;j <= y_right;++j) {
+      // Ans += nCr(i+j, i);
+    }
+  }
 }
