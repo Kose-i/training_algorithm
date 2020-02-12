34a8ec9e0b8c6d89e1db887305480fc38fcdab37
Kose-i <tamura.kosei905@mail.kyutech.jp>
2020-02-12 20:40:55 +0900

    Add ABC154-F

--- problem/AtCoder/AtCoderBeginnerContest154/F.cpp
+++ problem/AtCoder/AtCoderBeginnerContest154/F.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <vector>
 
 const int mod = 1000000007;
+using ll = long long;
 struct mint {
   ll x; // typedef long long ll;
   mint(ll x=0):x((x%mod+mod)%mod){}
@@ -49,13 +51,28 @@ struct mint {
     return res/=a;
   }
 };
+
 int main(int argc, char** argv) {
   unsigned x_left, y_left, x_right, y_right;
   std::cin >> x_left >> y_left >> x_right >> y_right;
-  mint Ans {};
-  for (auto i = x_left;i <= x_right;++i) {
-    for (auto j = y_left;j <= y_right;++j) {
-      // Ans += nCr(i+j, i);
-    }
+  --x_left;
+  --y_left;
+
+  constexpr int N {200005};
+  std::vector<mint> factorial(N+1, 1);
+  std::vector<mint> inverse_factorial(N+1, 1);
+  for (ll i = 1;i <= N;++i) {
+    factorial[i] = factorial[i-1]*i;
   }
+  inverse_factorial[N] = factorial[N].inv();
+  for (int i = N; i >= 1; --i) inverse_factorial[i-1] = inverse_factorial[i]*i;
+  auto g = [=](mint l, mint r) {
+    return factorial[l.x+r.x+2]*(inverse_factorial[l.x+1]*inverse_factorial[r.x+1]) - 1;
+  };
+
+  mint Ans = g(x_right, y_right);
+  Ans -= g(x_right, y_left);
+  Ans -= g(y_right, x_left);
+  Ans += g(x_left, y_left);
+  std::cout << Ans.x << '\n';
 }
