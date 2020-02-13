6d5c2fba27cd0a4dc3040dadd8b65367e7a2c898
Kose-i <tamura.kosei905@mail.kyutech.jp>
2020-02-13 17:54:02 +0900

    Add yukicoder-1

--- problem/yukiCoder/prob1.cpp
+++ problem/yukiCoder/prob1.cpp
@@ -6,13 +6,21 @@ struct Status{
   int have_money;
   int time_now;
 }
-unsigned answer(const int& country_size, const int& now_money, const int& path_size, const std::vector<int>& from_path, const std::vector<int>& to_path, const std::vector<int>& cost) {
+
+unsigned answer(const int& country_size, const int& now_money, const int& path_size, const std::vector<int>& from_path, const std::vector<int>& to_path, const std::vector<int>& cost, const std::vector<int>& time) {
   std::vector<std::vector<Status>> node(country_size);
-  Status initial_status;
-  initial_status.have_money = now_money;
-  initial_status.time_now = 0;
-  node[0].push_back(initial_status);
   /*ダイクストラ法*/
+  for (auto i = 0;i < country_size;++i) {
+    for (auto j = 0;j < country_size;++j) {
+      if (i == j) continue;
+      for (auto k = 0;k < country_size;++k) {
+        if (k == i || k == j) continue;
+        for (const auto& e : node[i][k]) {
+        //  node[i][j].push_back(e+ path[k][j]);
+        }
+      }
+    }
+  }
 }
 int main(int argc, char** argv) {
   int N, C, V;
@@ -22,6 +30,8 @@ int main(int argc, char** argv) {
   std::vector<int> to(V);
   for (auto& e : to) {std::cin >> e;}
   std::vector<int> cost(V);
-  for (auto& e : to) {std::cin >> e;}
-  std::cout << answer(N, C, V, from, to, cost) << '\n';
+  for (auto& e : cost) {std::cin >> e;}
+  std::vector<int> time(V);
+  for (auto& e : time) {std::cin >> e;}
+  std::cout << answer(N, C, V, from, to, cost, time) << '\n';
 }
