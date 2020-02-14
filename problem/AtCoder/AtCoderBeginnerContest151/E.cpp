63e76a0c68e5840bea4376c4f24dc0346ef94c72
Kose-i <tamura.kosei905@mail.kyutech.jp>
2020-02-14 19:12:27 +0900

    Add ABC151-E

--- problem/AtCoder/AtCoderBeginnerContest151/E.cpp
+++ problem/AtCoder/AtCoderBeginnerContest151/E.cpp
@@ -2,31 +2,87 @@
 #include <iostream>
 #include <vector>
 
-using namespace std;
+constexpr int mod {1000000007};
+struct mint {
+  using ll = long long;
+  ll x;
+  mint(ll x=0):x((x%mod+mod)%mod){}
+  mint operator-() const { return mint(-x);}
+  mint& operator+=(const mint a) {
+    if ((x += a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator-=(const mint a) {
+    if ((x += mod-a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator*=(const mint a) {
+    (x *= a.x) %= mod;
+    return *this;
+  }
+  mint operator+(const mint a) const {
+    mint res(*this);
+    return res+=a;
+  }
+  mint operator-(const mint a) const {
+    mint res(*this);
+    return res-=a;
+  }
+  mint operator*(const mint a) const {
+    mint res(*this);
+    return res*=a;
+  }
+  mint pow(ll t) const {
+    if (!t) return 1;
+    mint a = pow(t>>1);
+    a *= a;
+    if (t&1) a *= *this;
+    return a;
+  }
 
-constexpr long mod_target {1000000007};
+  // for prime mod
+  mint inv() const {
+    return pow(mod-2);
+  }
+  mint& operator/=(const mint a) {
+    return (*this) *= a.inv();
+  }
+  mint operator/(const mint a) const {
+    mint res(*this);
+    return res/=a;
+  }
+};
+constexpr long long N_max {100000};
+std::vector<mint> inv_factorial(N_max+1);
+std::vector<mint> factorial(N_max+1);
 
-long long nCm(int N, int M, vector<vector<long long>>& ncm_memo) {
-  if (N==M || M==0) return 1;
-  if (ncm_memo[N][M] != 0) return ncm_memo[N][M];
-  return ncm_memo[N][M] = nCm(N-1, M-1, ncm_memo) + nCm(N-1, M, ncm_memo);
+mint nCr(const long& n, const long& r) {
+  if (n < r) return 0;
+  return factorial[n]*inv_factorial[n-r]*inv_factorial[r];
 }
 
 int main(int argc, char** argv) {
   int N, K;
-  cin >> N >> K;
-  vector<long> A(N);
-  for (auto& e : A) cin >> e;
-  sort(A.begin(), A.end());
-  vector<vector<long long>> ncm_memo(N, vector<long long>(K, 0));
-
-  long long sum_A {};
-  for (auto i = 0;i < N;++i) {
-    long long min_elem = A[i];
-    for (auto j = i+K-1;j < N;++j) {
-      long long max_elem = A[j];
-      sum_A += (max_elem - min_elem) * nCm(j-i-1, K-2, ncm_memo)%mod_target;
-    }
+  std::cin >> N >> K;
+  std::vector<long long> A(N);
+  for (auto i = 0;i < N;++i) std::cin >> A[i];
+  std::sort(A.begin(), A.end()); // A[0] = smallest, A[N-1] = biggest
+  factorial[0].x = 1;
+  for (long long i = 1;i <= N;++i) {
+    factorial[i] = factorial[i-1]*i;
   }
-  cout << sum_A % mod_target << '\n';
+  for (long long i = 0;i <= N;++i) {
+    inv_factorial[i] = factorial[i].inv();
+  }
+  mint answer {};
+  for (long long i = N-1;i >= 0;--i) {
+    mint tmp = A[i];
+    answer += tmp*nCr(i, K-1);
+  }
+//  std::cout << answer.x << '\n';
+  for (long long i = 0;i < N;++i) {
+    mint tmp = A[i];
+    answer -= tmp*nCr(N-i-1, K-1);
+  }
+  std::cout << answer.x << '\n';
 }
