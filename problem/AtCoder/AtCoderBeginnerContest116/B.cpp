375610d385a995a1afa578276c47caea572631dd
Kose-i <tamura.kosei905@mail.kyutech.jp>
2020-04-18 14:01:59 +0900

    Add ABC116

---
+++ problem/AtCoder/AtCoderBeginnerContest116/B.cpp
@@ -0,0 +1,61 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <cmath>
+
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+
+constexpr ll LLINF {1001002003004005006};//ll = 9*LLINF
+constexpr int INTINF {1000000000};//int = 2*INTINF
+
+#define  rep(i,n)   for(int i = 0; i < (n); ++i)
+#define rrep(i,n)   for(int i = 1; i <= (n); ++i)
+#define drep(i,n)   for(int i = (n)-1; i >= 0; --i)
+#define srep(i,s,t) for(int i = s; i < t;  ++i)
+
+template<typename T>
+void maxs(T& x, T&& y) {
+  x=std::max(x,y);
+}
+template<typename T>
+void maxs(T& x, T& y) {
+  x=std::max(x,y);
+}
+
+template<typename T>
+void mins(T& x, T&& y) {
+  x=std::min(x,y);
+}
+template<typename T>
+void mins(T& x, T& y) {
+  x=std::min(x,y);
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  int s;
+  cin >> s;
+
+  map<int, char> p;
+  int tmp {s};
+  ull cnt {1};
+  while (true) {
+    if (p[tmp] == 0) {
+      p[tmp] = 1;
+    } else {
+      cout << cnt << '\n';
+      return 0;
+    }
+    if (tmp %2==0) {
+      tmp = tmp>>1;
+    } else {
+      tmp = tmp*3+1;
+    }
+    ++cnt;
+  }
+}
