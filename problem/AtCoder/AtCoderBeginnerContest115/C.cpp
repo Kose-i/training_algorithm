f986334d25ed873d26ad80f7afb4ed9d8c1deb22
Kose-i <tamura.kosei905@mail.kyutech.jp>
2020-04-19 14:07:50 +0900

    Add ABC115

---
+++ problem/AtCoder/AtCoderBeginnerContest115/C.cpp
@@ -0,0 +1,52 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <cmath>
+
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+
+constexpr ll LLINF {1001002003004005006};//ll = 9*LLINF
+constexpr int INTINF {1000000000};//int = 2*INTINF
+
+#define  rep(i,n)   for(int i = 0; i < (n); ++i)
+#define rrep(i,n)   for(int i = 1; i <= (n); ++i)
+#define drep(i,n)   for(int i = (n)-1; i >= 0; --i)
+#define srep(i,s,t) for(int i = s; i < t;  ++i)
+
+template<typename T>
+void maxs(T& x, T&& y) {
+  x=std::max(x,y);
+}
+template<typename T>
+void maxs(T& x, T& y) {
+  x=std::max(x,y);
+}
+
+template<typename T>
+void mins(T& x, T&& y) {
+  x=std::min(x,y);
+}
+template<typename T>
+void mins(T& x, T& y) {
+  x=std::min(x,y);
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  int N, K;
+  cin >> N >> K;
+  vector<ull> H(N);
+  rep(i, N) cin >> H[i];
+  std::sort(H.begin(), H.end());
+  ull answer {H[K-1] - H[0]};
+  rep(i, N) {
+    if (i+K>N) break;
+    mins(answer, H[i+K-1]-H[i]);
+  }
+  cout << answer << '\n';
+}
