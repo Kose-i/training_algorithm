ed7bc4e0420758749d75fb6c631c38abd51b85b7
Kose-i <tamura.kosei905@mail.kyutech.jp>
2020-05-05 22:27:55 +0900

    Add ABC165

---
+++ problem/AtCoder/AtCoderBeginnerContest165/A.cpp
@@ -0,0 +1,56 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <cmath>
+
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+
+constexpr ll LLINF {1001002003004005006};//ll = 9*LLINF
+constexpr int INTINF {1000000000};//int = 2*INTINF
+
+#define  rep(i,n)   for(int i = 0; i < (n); ++i)
+#define rrep(i,n)   for(int i = 1; i <= (n); ++i)
+#define drep(i,n)   for(int i = (n)-1; i >= 0; --i)
+#define srep(i,s,t) for(int i = s; i < t;  ++i)
+
+template<typename T>
+void maxs(T& x, T&& y) {
+  x=std::max(x,y);
+}
+template<typename T>
+void maxs(T& x, T& y) {
+  x=std::max(x,y);
+}
+
+template<typename T>
+void mins(T& x, T&& y) {
+  x=std::min(x,y);
+}
+template<typename T>
+void mins(T& x, T& y) {
+  x=std::min(x,y);
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+
+  int K, A, B;
+  cin >> K >> A >> B;
+  
+  if (A%K==0 || B%K==0) {
+    cout << "OK\n";
+    return 0;
+  }
+  int a = (A / K);
+  int b = (B / K);
+  if (a == b) {
+    cout << "NG\n";
+  } else {
+    cout << "OK\n";
+  }
+}
