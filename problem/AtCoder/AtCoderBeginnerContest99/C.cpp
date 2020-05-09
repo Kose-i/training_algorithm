90fe1eec302b7349ff7f183564334b039507341b
Kose-i <tamura.kosei905@mail.kyutech.jp>
2020-05-09 23:13:22 +0900

    Add ABC99

---
+++ problem/AtCoder/AtCoderBeginnerContest99/C.cpp
@@ -0,0 +1,64 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <cmath>
+
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+
+constexpr ll LLINF {1001002003004005006};//ll = 9*LLINF
+constexpr int INTINF {1000000000};//int = 2*INTINF
+
+#define  rep(i,n)   for(int i = 0; i < (n); ++i)
+#define rrep(i,n)   for(int i = 1; i <= (n); ++i)
+#define drep(i,n)   for(int i = (n)-1; i >= 0; --i)
+#define srep(i,s,t) for(int i = s; i < t;  ++i)
+
+template<typename T>
+void maxs(T& x, T&& y) {
+  x=std::max(x,y);
+}
+template<typename T>
+void maxs(T& x, T& y) {
+  x=std::max(x,y);
+}
+
+template<typename T>
+void mins(T& x, T&& y) {
+  x=std::min(x,y);
+}
+template<typename T>
+void mins(T& x, T& y) {
+  x=std::min(x,y);
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+
+  ll X;
+  cin >> X;
+
+  ll answer {};
+  for (ll x = X;x != 0;) {
+    ll max_c {1};
+    ll tmp {1};
+    while (tmp <= x) {
+      tmp *= 6;
+    }
+    tmp /= 6;
+    maxs(max_c, tmp);
+    tmp = 1;
+    while (tmp <= x) {
+      tmp *= 9;
+    }
+    tmp /= 9;
+    maxs(max_c, tmp);
+    x -= max_c;
+    ++answer;
+  }
+  cout << answer << '\n';
+}
