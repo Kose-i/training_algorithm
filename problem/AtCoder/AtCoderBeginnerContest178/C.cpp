e2eff1917a324b6d6a4f819dcc86389d1ee07623
Kose-i <tamura.kosei905@mail.kyutech.jp>
2020-09-14 16:25:38 +0900

    Add ABC178

---
+++ problem/AtCoder/AtCoderBeginnerContest178/C.cpp
@@ -0,0 +1,108 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <cmath>
+
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+
+constexpr ll LLINF {1001002003004005006};//ll = 9*LLINF
+constexpr int INTINF {1000000000};//int = 2*INTINF
+
+#define  rep(i,n)   for(int i = 0; i < (n); ++i)
+#define rrep(i,n)   for(int i = 1; i <= (n); ++i)
+#define drep(i,n)   for(int i = (n)-1; i >= 0; --i)
+#define srep(i,s,t) for(int i = s; i < t;  ++i)
+
+template<typename T>
+void maxs(T& x, T&& y) {
+  x=std::max(x,y);
+}
+template<typename T>
+void maxs(T& x, T& y) {
+  x=std::max(x,y);
+}
+
+template<typename T>
+void mins(T& x, T&& y) {
+  x=std::min(x,y);
+}
+template<typename T>
+void mins(T& x, T& y) {
+  x=std::min(x,y);
+}
+
+const int mod = 1000000007;
+struct mint {
+  ll x; // typedef long long ll;
+  mint(ll x=0):x((x%mod+mod)%mod){}
+  mint operator-() const { return mint(-x);}
+  mint& operator+=(const mint a) {
+    if ((x += a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator-=(const mint a) {
+    if ((x += mod-a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator*=(const mint a) {
+    (x *= a.x) %= mod;
+    return *this;
+  }
+  mint operator+(const mint a) const {
+    mint res(*this);
+    return res+=a;
+  }
+  mint operator-(const mint a) const {
+    mint res(*this);
+    return res-=a;
+  }
+  mint operator*(const mint a) const {
+    mint res(*this);
+    return res*=a;
+  }
+  mint pow(ll t) const {
+    if (!t) return 1;
+    mint a = pow(t>>1);
+    a *= a;
+    if (t&1) a *= *this;
+    return a;
+  }
+
+  // for prime mod
+  mint inv() const {
+    return pow(mod-2);
+  }
+  mint& operator/=(const mint a) {
+    return (*this) *= a.inv();
+  }
+  mint operator/(const mint a) const {
+    mint res(*this);
+    return res/=a;
+  }
+};
+
+int main() {
+  ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+
+  int N;
+  cin >> N;
+
+  mint a(10);
+  a = a.pow(N);
+  mint b(9);
+  b = b.pow(N);
+  b *= 2;
+  mint c(8);
+  c = c.pow(N);
+
+  mint ans = a + c - b;
+  //cout << "a:" << a.x << '\n';
+  //cout << "b:" << b.x << '\n';
+  //cout << "c:" << c.x << '\n';
+  cout << ans.x << '\n';
+}
