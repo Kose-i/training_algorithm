5cc8bf4860dd078ac2d5b132868ede15a63d2c6a
Kose-i <tamura.kosei905@mail.kyutech.jp>
2020-10-24 18:41:28 +0900

    Add CodeIQ-book1.prob2

--- problem/CodeIq/book1/problem2.cpp
+++ problem/CodeIq/book1/problem2.cpp
@@ -1,40 +1,65 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
 
-struct Num {
-  public:
-    Num(int c) : num(c), den(1){}
-    Num op(const Num& l, const Num& r, char)
-    int to_int() {
-      if (num%den != 0) return -1;
-      return num/den;
-    }
-  private:
-    int den, num;
-};
+//8*10^4
 
-int eval(const int& i, const char& c1, const char& c2, const char& c3) {
-  if (c1=='~' && c1==c2 && c1==c3) return -1;
-  std::vector<Num> Num_vec;
-  std::vector<char> op_vec;
-  int Num_size = Num_vec.size();
-  Num ans(Num_vec[0]);
-  for (auto i = 1;i < Num_size;++i) {
-    ans = op(ans, Num_vec[i], op_vec[i-1]);
-  }
-  return ans.to_int();
+bool is_equal(std::string s1, std::string s2) {
+  //std::sort(s1.begin(), s1.end());
+  //std::sort(s2.begin(), s2.end());
+  return s1==s2;
 }
 
+std::vector<std::string> split(const std::string& s, const char& t) {
+  std::vector<std::string> ans;
+  std::string str{};
+  for (auto i =0;i < s.size();++i) {
+    if (s[i] == t) {
+      ans.push_back(str);
+      str = "";
+    } else {
+      str += s[i];
+    }
+  }
+  ans.push_back(str);
+  return ans;
+}
+
+std::string eval(const std::string& s) {
+  std::vector<std::string> split_s = split(s, '*');
+  int num {1};
+  for (const auto& e : split_s) {
+    num *= stoi(e);
+  }
+  return std::to_string(num);
+}
+bool is_check(char num1, char num2, char num3, char num4) {
+  const std::vector<char> op {'*', ' '};
+  for (const char& a : op) {
+    for (const char& b : op) {
+      for (const char& c : op) {
+        std::string s {};
+        s += num1;
+        if (a != ' ') s += a;
+        s += num2;
+        if (b != ' ') s += b;
+        s += num3;
+        if (c != ' ') s += c;
+        s += num4;
+        if (a == b && b == c && a == ' ') continue;
+        std::string tmp_s {num4, num3, num2, num1};
+        if (is_equal(eval(s), tmp_s)) std::cout << s << '\n';
+      }
+    }
+  }
+  return true;
+}
 int main(int argc, char** argv) {
-  std::vector<char> op {'+', '-', '*', '/', '~'};
-  for (int i = 1000;i < 10000;++i) {
-    for (auto e : op) {
-      for (auto f : op) {
-        for (auto g : op) {
-          if (eval(i, e, f, g) == i) {
-            std::cout << i << '\n';
-            return 0;
-          }
+  for (char num1 = '1';num1 <= '9';++num1) {
+    for (char num2 = '0';num2 <= '9';++num2) {
+      for (char num3 = '0';num3 <= '9';++num3) {
+        for (char num4 = '0';num4 <= '9';++num4) {
+          if (is_check(num1, num2, num3, num4)) ;//std::cout << "kd\n";
         }
       }
     }
